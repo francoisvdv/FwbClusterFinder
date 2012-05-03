c975841f12d8b00948ef197f69f86b02283d8047
Francois van der Ven <francoisvdven@gmail.com>
2012-05-03 14:46:30 +0200

    Removed getters/setters in Point class.

--- FwbAlgorithm/src/Point.java
+++ FwbAlgorithm/src/Point.java
@@ -4,31 +4,6 @@ public class Point
 	int y;
 	int cluster;
 	
-	public int getX()
-	{
-		return x;
-	}
-	public void setX(int x)
-	{
-		this.x = x;
-	}
-	public int getY()
-	{
-		return y;
-	}
-	public void setY(int y)
-	{
-		this.y = y;
-	}
-	public int getCluster()
-	{
-		return cluster;
-	}
-	public void setCluster(int cluster)
-	{
-		this.cluster = cluster;
-	}
-	
 	public Point(int x, int y)
 	{
 		this.x = x;
