6e0f054bc9a609766796df93df551aaf418a4937
Francois van der Ven <francoisvdven@gmail.com>
2012-05-11 14:05:50 +0200

    Progress on Threshold

--- FwbAlgorithm/src/TThreshold.java
+++ FwbAlgorithm/src/TThreshold.java
@@ -14,17 +14,18 @@ public class TThreshold
 		}
 	}
 
-	int stepCount = 100;
+	int stepCount = 100; //TODO: veranderen door testen
 	
 	int previousNumberOfPoints;
 	int numberOfPoints;
 	float currentThreshold;
 	float maxThreshold;
 
-	//step = MaxThreshold / 100
-	//
-	
-	public void FindSwitches()
+	/**
+	 * Returns a Threshold value. Everything BELOW this threshold should be cut off (noise).
+	 * @return
+	 */
+	public float FindThreshold()
 	{
 		//Tussen element 0 en 1 zit een switch, tussen element 2 en 3 een switch, etc.
 		ArrayList<Float> switches = new ArrayList<Float>();
@@ -64,10 +65,51 @@ public class TThreshold
 		 * We detecteren of de laatste cluster veel 'steps' bevat in vergelijking met de rest, en de cluster
 		 * die daarvoor komt kunnen we samenvoegen wanneer de hoeveelheid steps tussen die twee klein is.
 		 */
-	}
-	
-	public void RecursiveMethod(float threshold)
-	{
 		
+		boolean noiseDetected = false;
+		
+		//Merge last thresholds		
+		float mergeThreshold = (float)Math.PI / 100 * maxThreshold; //TODO: aanpassen door testen
+		for(int i = switches.size() - 1; i >= 0; i -= 2)
+		{
+			if(Math.abs(switches.get(i) - switches.get(i - 1)) > mergeThreshold)
+				break; //Als het een cluster is dan breakt ie altijd. Als het noise is kán hij breaken.
+			
+			//Als hij niet breekt is het sowieso noise.
+			noiseDetected = true;
+			
+			switches.remove(i);
+			switches.remove(i - 1);
+		}
+		
+		if(noiseDetected) //we hebben sowieso noise, dus laatste eraf halen
+		{
+			//Hak laatste eraf
+			switches.remove(switches.size());
+			switches.remove(switches.size());
+		}
+		else //het is niet zeker of we noise hebben, bepaal dit
+		{
+			float totalDifference = 0;
+			int totalSteps = 0;
+			for(int i = 0; i < switches.size() - 2; i += 2)
+			{
+				totalDifference += Math.abs(switches.get(i) + switches.get(i + 1));
+				totalSteps++;
+			}
+			
+			//de average van alle switches behalve de laatste
+			int averageSteps = (int)Math.ceil(totalDifference / totalSteps);
+			float maximalDeviation = averageSteps * 1.4f; //TODO: Deviation 1.4f bepalen door testen
+			
+			if(Math.abs(switches.get(switches.size()) - switches.get(switches.size() - 1)) > maximalDeviation)
+			{
+				//Laatste is noise dus die hakken we eraf
+				switches.remove(switches.size());
+				switches.remove(switches.size());
+			}
+		}
+		
+		return switches.get(switches.size());
 	}
 }
