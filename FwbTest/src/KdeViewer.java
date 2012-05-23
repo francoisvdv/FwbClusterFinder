ab2342ca94826e4b8acce97ea91606f46e2885c0
Francois van der Ven <francoisvdven@gmail.com>
2012-05-23 11:46:18 +0200

    Meer kleurdingen getest

--- FwbTest/src/KdeViewer.java
+++ FwbTest/src/KdeViewer.java
@@ -30,6 +30,8 @@ public final class KdeViewer
 		
 		BufferedImage img = new BufferedImage(in.width, in.height, BufferedImage.TYPE_INT_RGB);
 		Graphics g = img.createGraphics();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, img.getWidth(), img.getHeight());
 		
 		for(int i = 0; i < in.width; i++)
 		{
@@ -55,6 +57,7 @@ public final class KdeViewer
 	
 	static Color densityToColor(float density, float maxDensity)
 	{
+		//Kleuring 1:
 		//bottom is green, middle is blue, top is red
 		float rel = Math.max(0, Math.min(density / maxDensity, 1));
 		Color c = Color.BLACK;
@@ -65,6 +68,35 @@ public final class KdeViewer
 			c = new Color(0, 0, (rel - (1/3f)) / (1/3f));
 		else if(rel > (2/3f))
 			c = new Color((rel - (2/3f)) / (1/3f), 0, 0);
+		
+//		//Kleuring 2:
+//		float h = (1 - rel) * (80/360f) - (20/360f); //red to yellow
+//		if(h < 0)
+//			h = 1 - h;
+//		float s = 1;
+//		float v = 1;//rel == 0 ? 0 : 1;
+//
+//		c = Color.getHSBColor(h, s, v);
+//
+//		//Kleuring 3:
+//		int r = 0, g = 0,b = 0;
+//		if(rel <= (1/2f))
+//		{
+//			g = 0;
+//			r = (int)((rel / 0.5f) * 255);
+//			b = 0;
+//		}
+//		else
+//		{
+//			g = (int)(((rel - 0.5f) / 0.5f) * 255);
+//			r = 255;
+//			b = 0;
+//		}
+//		
+//		r = Math.max(0, Math.min(r, 255));
+//		g = Math.max(0, Math.min(g, 255));
+//		
+//		c = new Color(r, g, b);
 		
 		return c;
 	}
