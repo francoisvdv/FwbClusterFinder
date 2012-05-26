4759e0cdea2f785740815f0580ccdb166b77b301
Huib <huibdonkers@aol.com>
2012-05-26 20:21:02 +0200

    - bandwidth heeft beter waardes (boven de 1000 punten) - grootte van scaledField wordt afgesteld op bandwidth - sorting werkt beter (niet altijd goed)
    Ik zal wel een hoop conflicten krijgen

--- FwbAlgorithm/src/Constants.java
+++ FwbAlgorithm/src/Constants.java
@@ -3,10 +3,10 @@ public final class Constants
 	public final class KDE
 	{
 		public static final float BWRADIUS    = 2.5f;
-		public static final float BWFACTOR    = 50f;
+		public static final float BWFACTOR    = 2f;
 		public static final int   MAXGRIDSIZE = 1000;
-		public static final int   CELLS_PER_BANDWIDTH
-		                                      = 50;
+		public static final float CELLS_PER_BANDWIDTH
+		                                      = 4;
 	}
 	
 	public final class Threshold
